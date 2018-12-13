30de3f8f79c749f085c6087eaf9d8e7dae18de30
Martin Skarzynski <marskar@gmail.com>
2018-12-13 14:29:15 -0500

    edit slides_transfer script and make slide.html

--- slides_transfer.py
+++ slides_transfer.py
@@ -1,34 +1,67 @@
 import pypandoc
+import doctest
 
 
-def write_file(filename, contents):
+def write_file(filename: str, contents: str) -> None:
+    """Writes contents to a file named filename
+
+    Args:
+        filename: The name of the target file
+        contents: The contents of the target file
+
+    Examples:
+        >>> import tempfile
+        >>> outfile_path = tempfile.mkstemp()[1]
+        >>> write_file(outfile_path, "Test file contents")
+        >>> with open(outfile_path) as file: file.read()
+        'Test file contents'
+    """
     with open(filename, 'w') as f:
         f.write(contents)
 
 
-# With an input file: pypandoc will infer the input format from the filename
-output = pypandoc.convert_file('Presentation.md'', to='rst')
-write_file('Presentation.rst'', output)
+def make_slides(path: str = 'slides.md', framework: str = 'slidy') -> str:
+    """Writes contents to a file named filename
 
-# You can make a slidy HTML slide deck
-slides_HP = pypandoc.convert_file('Presentation.md', to='slidy', extra_args=['-s'])
+    Args:
+        path: The filepath of the target file
+        framework: The HTML slideshow type, must be revealjs, slidy, or dzslides
 
-# And specify style with a css file, e.g. https://github.com/alblaine/countess
-slides_HP = pypandoc.convert_file('Presentation.md', to='slidy', extra_args=['-s', '--css=style.css'])
+    Returns:
+        A string of characters that are the contents of an html slideshow.
 
-# Another option is to make a dzslides HTML slide deck
-slides_HP = pypandoc.convert_file('Presentation.md', to='dzslides', extra_args=['-s'])
+    Raises:
+        TypeError: The framework argument must be passed as string.
+        ValueError: Only three html slide frameworks: revealjs, slidy, or dzslides,
+            are currently supported.
 
-# And now with a css style: https://gist.github.com/bjin/2264634
-slides_HP = pypandoc.convert_file('Presentation.md', to='dzslides', extra_args=['-s', '--css=slide.css'])
+    Examples:
+        A minimal example using temporary input and output files
+        >>> import tempfile # library needed to make temp files
+        >>> infile_path = tempfile.mkstemp('.md')[1] # markdown file path
+        >>> write_file(infile_path, "# Markdown header") # markdown content
+        >>> outfile_path = tempfile.mkstemp(suffix='.html')[1] # html file path
+        >>> write_file(outfile_path, make_slides(infile_path)) # html slides
+        >>> with open(outfile_path, "r") as f: outfile_contents = f.read()
+        >>> lines = outfile_contents.split('\\n') # split contents into lines
+        >>> lines[0] # first line
+        '<?xml version="1.0" encoding="utf-8"?>'
+        >>> lines[-4] # fourth to last line
+        '<div id="markdown-header" class="title-slide slide section level1"><h1>Markdown header</h1></div>'
+    """
+    if type(framework) is str:
+        if framework in ('slidy', 'dzslides'):
+            return pypandoc.convert_file(path, to=framework, extra_args=['-s'])
+        elif framework == 'revealjs':
+            return pypandoc.convert_file(path, to=framework,
+                                         extra_args=['-s', '-V', 'revealjs-url=http://lab.hakim.se/reveal-js'])
+        else:
+            raise ValueError("Framework argument must be 'revealjs', 'slidy', or 'dzslides', not {framework}.")
+    else:
+        raise TypeError("Framework argument type must be str, not {type(framework)}.")
 
-# You can also make a revealjs HTML slide deck
-slides_HP = pypandoc.convert_file('Presentation.md', to='revealjs', extra_args=['-s', '-V', 'revealjs-url=http://lab.hakim.se/reveal-js'])
 
-# Try the sky revealjs theme
-slides_HP = pypandoc.convert_file('Presentation.md', to='revealjs', extra_args=['-s', '-V', 'revealjs-url=http://lab.hakim.se/reveal-js', '-V', 'theme=sky'])
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
+    write_file('slides.html', make_slides('Presentation.md'))
 
-# Try the moon revealjs theme
-slides_HP = pypandoc.convert_file('Presentation.md', to='revealjs', extra_args=['-s', '-V', 'revealjs-url=http://lab.hakim.se/reveal-js', '-V', 'theme=moon'])
-
-write_file('Presentation_HPL.html', slides_HP)
